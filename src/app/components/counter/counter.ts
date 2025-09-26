3475bf07758b67e7a7c4f618ecd5ef4a41be96cd
redaalch <s133260695@taalim.ma>
2025-09-26 22:31:14 +0100

    counter app

--- src/app/components/counter/counter.ts
+++ src/app/components/counter/counter.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter.html',
-  styleUrl: './counter.scss'
+  styleUrl: './counter.scss',
 })
 export class Counter {
-
+  counterValue = signal(0);
+  increment() {
+    this.counterValue.set(this.counterValue() + 1);
+  }
+  decrement() {
+    this.counterValue.update((val) => val - 1);
+  }
+  reset() {
+    this.counterValue.set(0);
+  }
 }
